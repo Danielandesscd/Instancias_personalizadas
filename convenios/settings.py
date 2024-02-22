9a672fb435af489994ba2a04b8314e95df765264
Danielandesscd <daniel.ortiz@andesscd.com.co>
2024-02-22 16:21:28 -0500

    DTB

--- convenios/settings.py
+++ convenios/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'instancias',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,14 @@ WSGI_APPLICATION = 'convenios.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instancias',
+        'USER': 'postgres',
+        'PASSWORD': 'Colombia2023**',
+        'HOST': 'localhost',  
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
