a519ce95d57641f2d358017ab9d2eecce4ba940e
Danielandesscd <daniel.ortiz@andesscd.com.co>
2024-02-29 11:47:41 -0500

    almacenamiento form

--- convenios/urls.py
+++ convenios/urls.py
@@ -7,12 +7,12 @@ from django.contrib.auth import views as auth_views
 from instancias import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('',views.inicio, name='inicio'),
-
     path('instancias', views.instancia, name='instancia'),
     path('home', views.home, name='home'),
     path('formulario/', views.formulario_instancia, name='formulario'),
     path('cerrar-sesion/', auth_views.LogoutView.as_view(next_page='inicio'), name='cerrar_sesion'),
+    path('crear_instancia/', views.crear_instancia, name='crear_instancia'),
+
 
 ]
